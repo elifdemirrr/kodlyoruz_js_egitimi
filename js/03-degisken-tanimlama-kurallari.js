157d6c2fcfaae24116163b450d6845f8e9a18841
elifdemirrr <elifdemir6020@gmail.com>
2024-09-17 18:28:13 +0300

    degisken tanimlama kurallari

---
+++ js/03-degisken-tanimlama-kurallari.js
@@ -0,0 +1,28 @@
+    let fullname  = "" 
+    let kodluyoruzserverinfo = "" 
+    let kodluyoruz_server_info = "" 
+    let fullName = ""
+    let kodluyoruzServerInfo = "" 
+    let kodluyoruzSERverInfo = "" 
+    
+    let firstName = "Elif"
+    let lastName = "Demir"
+    console.log(firstName, lastName)
+    
+    // UPPER_CASE Kullanimi:
+    const password = "1234" 
+    const PASSWORD = "1234" 
+    const SERVER_PASSWORD = "1234"
+    
+    let türkçeBilgi = 'Türkçe Bilgi Yazdırılıyor..'
+    let info = 'Türkçe Bilgi Yazdırılıyor..'
+    console.log(info)
+    
+ 
+    let x = 1;  
+    
+    
+    let isActive = true
+    let hasPassword = false
+    
+    
