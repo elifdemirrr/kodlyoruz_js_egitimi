45792496d4bbd8b9a3c5cc303440efed1907af5d
elifdemirrr <elifdemir6020@gmail.com>
2024-09-17 18:21:29 +0300

    degisken tanimlama

---
+++ js/02-degisken-tanimlama.js
@@ -0,0 +1,30 @@
+
+let serverName;
+console.log(serverName)
+
+serverName = "https://kodluyoruz.org"
+console.log(serverName)
+
+let password = "1234";
+console.log(password)
+
+let fullName = "Elif Demir";
+
+
+fullName = "Lorem Ipsum Dolor"
+console.log(fullName)
+
+fullName + "Yeni Eklenen Bilgi" 
+console.log(fullName + " Test Bilgisi") 
+
+fullName = fullName + " Yeni Bilgi"
+fullName =  "2: Bilgi : " + fullName
+
+fullName = "Sifirlandi"
+fullName += " ve Yeni Bilgi Eklendi"
+console.log(fullName)
+
+
+const SERVER_PASSWORD = "wfeverbrtbrthrth"
+console.log(SERVER_PASSWORD)
+
