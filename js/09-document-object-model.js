1410e65b14727bed3e643351f3095b44b64350f2
elifdemirrr <elifdemir6020@gmail.com>
2024-09-20 01:12:48 +0300

    document object model

---
+++ js/09-document-object-model.js
@@ -0,0 +1,8 @@
+// Reacing the dom objects
+console.log(document.location.pathname)
+console.log(document.body)
+console.log(document.head)
+console.log(document.URL)
+console.log(document.baseURI)
+
+document.body.style.backgroundColor = "pink"
