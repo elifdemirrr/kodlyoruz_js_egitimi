3b34aa3a4f2c2e8486d7637a06ee4b3400ff5f26
elifdemirrr <elifdemir6020@gmail.com>
2024-09-24 20:42:06 +0300

    kosullarla calismak

---
+++ js/015-kosullarla-calismak.js
@@ -0,0 +1,14 @@
+let userName = prompt("Kullanici adini giriniz:")
+
+// eger kullanıcı adı varsa ekrana yazdır
+//if (kullaniciadi) varsa ekrana (kullaniciadı) yazdır.
+
+if(userName.length > 3){
+    console.log(`Kullanıcı Adınız: ${userName}`)
+}
+else {
+    console.log(`Bilgi yok`)
+}
+// kullanıcı adı yoksa ekrana bilgi yok yazdır.
+// else ekrana bilgi yok yazdır
+
