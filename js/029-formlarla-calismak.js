63648a7e46fdd92f7de9991aae417ce715f155d8
elifdemirrr <elifdemir6020@gmail.com>
2024-10-11 00:52:45 +0300

    main branchındayım ve elife gitmeyecek degisikler

---
+++ js/029-formlarla-calismak.js
@@ -0,0 +1,11 @@
+//deneme şuan main deyim ve gite atıcam deneyeceğim elif branchına gitmiş mi gitmemiş mi?
+//everything for My learning X wrong
+//everything is for me to learn  V correct
+
+
+let userFormDOM = document.querySelector('#userForm')
+userFormDOM.addEventListener('submit', formHandler)
+
+function formHandler(event) {
+    event.preventDefault()
+}
